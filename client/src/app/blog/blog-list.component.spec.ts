de40c156e95bf31a183948861863337ac9b4fb0e
Javier Pena <javier@javierdev.com>
2017-03-21 11:39:18 -0500

    Add bloglist spec for paging method

--- client/src/app/blog/blog-list.component.spec.ts
+++ client/src/app/blog/blog-list.component.spec.ts
@@ -38,7 +38,7 @@ describe('BlogListComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            queryParams: Observable.from([1])
+            queryParams: Observable.from([1]),
             snapshot: {
               data: {blogList: {blogEntries: blogListStub}},
               queryParams: {limit: 10, sort: '-created', skip: 0}
@@ -68,5 +68,53 @@ describe('BlogListComponent', () => {
   it('initializes with blog list from route resolve', () => {
     component.ngOnInit();
     expect(component.blogEntries).toEqual(blogListStub);
+  });
+
+  describe('changePage', () => {
+    let queryParams;
+    let router;
+
+    beforeEach(() => {
+      router = TestBed.get(Router);
+      spyOn(router, 'navigate');
+    });
+
+    it('navigates using existing route\'s params by default', () => {
+      component.ngOnInit();
+      const route = TestBed.get(ActivatedRoute);
+      queryParams = route.snapshot.queryParams;
+      queryParams.page = 1;
+      expect(router.navigate).not.toHaveBeenCalled();
+      component.changePage();
+      expect(router.navigate)
+        .toHaveBeenCalledWith(['/blog/list'], {queryParams});
+    });
+    
+    beforeEach(() => {
+      component.ngOnInit();
+      component.limit = 20;
+      component.sort = '-title';
+      queryParams = {
+        page: 1,
+        sort: component.sort,
+        limit: component.limit,
+        skip: 0
+      };
+    });
+
+    it('navigates using queryParams and default page (1)', () => {
+      expect(router.navigate).not.toHaveBeenCalled();
+      component.changePage();
+      expect(router.navigate)
+        .toHaveBeenCalledWith(['/blog/list'], {queryParams});
+    });
+
+    it('navigates using queryParams and input page', () => {
+      expect(router.navigate).not.toHaveBeenCalled();
+      component.changePage({page: 5});
+      queryParams.page = 5;
+      expect(router.navigate)
+        .toHaveBeenCalledWith(['/blog/list'], {queryParams});
+    });
   });
 });
