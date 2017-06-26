4bbd80fb7b8b0e8a1e406817939bf4aebf884cbb
Javier Pena <javier@javierdev.com>
2017-06-26 10:49:34 -0500

    #29 Preload blog module

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadingStrategy, RouterModule, Route, Routes } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 // Add lazy-loaded routes/modules here
 const APP_ROUTES: Routes = [
@@ -9,16 +11,31 @@ const APP_ROUTES: Routes = [
   },
   {
     path: 'blog',
-    loadChildren: './blog/blog.module#BlogModule'
+    loadChildren: './blog/blog.module#BlogModule',
+    data: {
+      preload: true
+    }
   }
 ];
 
+// Custom preloading strategy. Preloads routes with data: {preload: true}
+export class CustomPreloadingStrategy implements PreloadingStrategy {
+  preload(route: Route, load: Function): Observable<any> {
+    return route.data && route.data['preload'] ? load() : Observable.of(null);
+  }
+}
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES, {
+      preloadingStrategy: CustomPreloadingStrategy
+    })
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    CustomPreloadingStrategy
   ]
 })
 export class AppRoutingModule {}
